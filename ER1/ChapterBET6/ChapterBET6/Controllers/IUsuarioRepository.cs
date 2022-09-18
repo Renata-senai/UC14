0065976a4024e75c496f044135dff02c8302d94d
Renata-senai <94024541+Renata-senai@users.noreply.github.com>
2022-09-18 15:01:01 -0300

    ER4

---
+++ ER1/ChapterBET6/ChapterBET6/Controllers/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using API_LivroSENAI.Models;
+using ChapterProject1.Models;
+
+namespace API_LivroSENAI.Controllers
+{
+    internal class IusuarioRepository
+    {
+        internal Usuario Login(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
